84f58bd18f86718076116b3413a46b4e14eb1669
danghdkhoa <danghuynhdangkhoa13@gmail.com>
2025-11-30 16:39:13 +0700

    add protected mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,30 +1,80 @@
-[BITS 16]         ; Set the code to 16-bit mode
-[ORG 0x7c00]      
+[BITS 16]
+[ORG 0x7c00]
+
+CODE_OFFSET equ 0x8
+
+DATA_OFFSET equ 0x10
 
 start:
-    cli           
-    mov ax, 0x00  
-    mov ds, ax    
-    mov es, ax    
-    mov ss, ax    
+    cli
+
+    mov ax, 0x00
+
+    mov ds, ax
+
+    mov es, ax
+
+    mov ss, ax
+
     mov sp, 0x7c00
-    mov si, msg   
-    sti           
 
-print:
-    lodsb         
-    cmp al, 0     
-    je done       
-    mov ah, 0x0E  
-    int 0x10      
-    jmp print     
+    sti
 
-done:
-    cli           ; Clear interrupts
-    hlt           
+load_PM:
+    cli
 
-msg: db 'Hello World!', 0 
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
 
-times 510 - ($ - $$) db 0 
+    jmp CODE_OFFSET:PModeMain
 
-dw 0xAA55     
+gdt_start:
+    dd 0x0
+    dd 0x0
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+
+[BITS 32]
+
+PModeMain:
+    mov ax, DATA_OFFSET
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov ss, ax
+    mov gs, ax
+    mov ebp, 0x9C00
+    mov esp, ebp
+
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    jmp $
+
+
+times 510 - ($ - $$) db 0
+
+
+dw 0xAA55
