05e854153fba91e207b507142676c551d0787c43
danghdkhoa <danghuynhdangkhoa13@gmail.com>
2025-11-30 17:08:23 +0700

    load kernel.c

--- src/boot.asm
+++ src/boot.asm
@@ -1,62 +1,80 @@
-[BITS 16]
-[ORG 0x7c00]
+[BITS 16]        ; Set the code to 16-bit mode
+[ORG 0x7c00]     ; Set the origin (starting address) to 0x7c00, typical for boot loaders
+
 
 CODE_OFFSET equ 0x8
-
 DATA_OFFSET equ 0x10
 
+KERNEL_LOAD_SEG equ 0x1000
+KERNEL_START_ADDR equ 0x100000
+
+
+
 start:
-    cli
+    cli           ; Clear interrupts, disabling all maskable interrupts
+    mov ax, 0x00  ; Load immediate value 0x00 into register AX
+    mov ds, ax    ; Set data segment (DS) to 0x00
+    mov es, ax    ; Set extra segment (ES) to 0x00
+    mov ss, ax    ; Set stack segment (SS) to 0x00
+    mov sp, 0x7c00; Set stack pointer (SP) to 0x7c00, top of the bootloader segment
+    sti           ; Enable interrupts, allowing them to occur again
+    
 
-    mov ax, 0x00
+;Load kernel
+mov bx, KERNEL_LOAD_SEG
+mov dh, 0x00
+mov dl, 0x80
+mov cl, 0x02
+mov ch, 0x00
+mov ah, 0x02
+mov al, 8
+int 0x13
 
-    mov ds, ax
+jc disk_read_error
 
-    mov es, ax
-
-    mov ss, ax
-
-    mov sp, 0x7c00
-
-    sti
 
 load_PM:
     cli
-
-    lgdt [gdt_descriptor]
+    lgdt[gdt_descriptor]
     mov eax, cr0
     or al, 1
     mov cr0, eax
-
     jmp CODE_OFFSET:PModeMain
+
+
+disk_read_error:
+    hlt
+
+;GDT Implemetation
 
 gdt_start:
     dd 0x0
     dd 0x0
 
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 10011010b
-    db 11001111b
-    db 0x00
+    ; Code segment descriptor
+    dw 0xFFFF       ; Limte
+    dw 0x0000       ; Base
+    db 0x00         ; Base
+    db 10011010b    ; Access byte
+    db 11001111b    ; Flags
+    db 0x00         ; Base
 
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 10010010b
-    db 11001111b
-    db 0x00
+    ; Data segment descriptor
+    dw 0xFFFF       ; Limte
+    dw 0x0000       ; Base
+    db 0x00         ; Base
+    db 10010010b    ; Access byte
+    db 11001111b    ; Flags
+    db 0x00         ; Base
 
 gdt_end:
 
 gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
+    dw gdt_end - gdt_start - 1 ; Size of GDT -1
+    dd gdt_start 
 
 
 [BITS 32]
-
 PModeMain:
     mov ax, DATA_OFFSET
     mov ds, ax
@@ -71,10 +89,11 @@ PModeMain:
     or al, 2
     out 0x92, al
 
-    jmp $
+    jmp CODE_OFFSET:KERNEL_START_ADDR
 
 
-times 510 - ($ - $$) db 0
 
 
-dw 0xAA55
+times 510 - ($ - $$) db 0   ; Fill the rest of the boot sector with zeros up to 510 bytes
+
+dw 0xAA55   ; Boot sector signature, required to make the disk bootable
